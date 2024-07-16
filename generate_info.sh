76d3aa3fe79dce68134e79e9d8ceb8acec1a7bc7
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 12:25:38 +0200

    feat: echo filename, calc number of lines

--- generate_info.sh
+++ generate_info.sh
@@ -7,3 +7,7 @@ for file in in/*.txt; do
     base_name=$(basename "$file")
     output_file="out/info_${base_name}"
     
+    echo "~~ $base_name info ~~" >> "$output_info"
+
+    echo "Number of lines:" >> "$output_info"
+    cat "$file" | wc -l >> "$output_info"
