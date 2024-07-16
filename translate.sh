a1ca0a4a0f0e3e68f706252b52e44f51c95866f8
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 12:21:13 +0200

    test: results at out folder. passed tests

--- translate.sh
+++ translate.sh
@@ -4,6 +4,6 @@ mkdir -p out
 
 for file in in/*.txt; do
     base_name=$(basename "$file")
-    output_file="out/doggy_${base_name}"
+    output_file="out/translated_${base_name}"
     cat "$file" | sed -E 's/catnip/dogchow/g; s/cat/dog/g; s/meow|meowzer/woof/g' > "$output_file"
 done
